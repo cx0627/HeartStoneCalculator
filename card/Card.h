4e80f9817cc5c30394f98deeade6e604482a3c6c
sugar <2919730935@qq.com>
2022-08-27 22:29:29 +0800

    to #1, add Card.h

--- card/Card.h
+++ card/Card.h
@@ -1,7 +1,42 @@
 //
 // Created by sugar on 2022/8/24.
 //
+#ifndef HEARTSTONECALCULATOR_Card_H
+#define HEARTSTONECALCULATOR_Card_H
 
-class Card {
+#include <String.h>
+#include <vector>
+#include "Handle.h"
+using std::string;
+using std::vector;
 
+class Card
+{
+private:
+    string name;
+    vector<Handle> handlerList;
+
+public:
+    string getName()
+    {
+        return name;
+    }
+
+    int addHandle(Handle handle)
+    {
+        handlerList.push_back(handle);
+        return handlerList.size();
+    }
+
+    int getHandleListLen()
+    {
+        return handlerList.size();
+    }
+
+    void ClearHandleList()
+    {
+        handlerList.clear();
+    }
 };
+
+#endif //HEARTSTONECALCULATOR_Card_H
